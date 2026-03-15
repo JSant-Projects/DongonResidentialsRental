2b767ab92ba078374c25fb68c1415233f3870e02
jayson <jayson.santiago1216@gmail.com>
2026-03-14 19:52:33 -0600

    [Application.Units] Added GetAvailableUnitsLookupByBuildingQuery

---
+++ DongonResidentialsRental.Application/Units/Queries/GetAvailableUnitsLookupByBuilding/GetAvailableUnitsLookupQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using DongonResidentialsRental.Application.Abstractions.Data;
+using DongonResidentialsRental.Application.Abstractions.Messaging;
+using DongonResidentialsRental.Application.Units.Queries.GetUnits;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DongonResidentialsRental.Application.Units.Queries.GetAvailableUnitsLookupByBuilding;
+
+public sealed class GetAvailableUnitsLookupQueryHandler : IQueryHandler<GetAvailableUnitsLookupByBuildingQuery, IReadOnlyList<UnitLookupResponse>>
+{
+    private readonly IApplicationDBContext _dbContext;
+    public GetAvailableUnitsLookupQueryHandler(IApplicationDBContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task<IReadOnlyList<UnitLookupResponse>> Handle(GetAvailableUnitsLookupByBuildingQuery request, CancellationToken cancellationToken)
+    {
+        var lookup = await _dbContext.Units
+            .AsNoTracking()
+            .Where(u => 
+                u.BuildingId == request.BuildingId 
+                && u.Status == Domain.Unit.UnitStatus.Available)
+            .Select(UnitMappings.ToLookupResponse())
+            .ToListAsync(cancellationToken);
+
+        return lookup;
+    }
+}
