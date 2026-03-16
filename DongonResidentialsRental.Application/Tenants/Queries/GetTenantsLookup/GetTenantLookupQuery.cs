80a4d1f48dff8fdcc10d4e5dc74b7c8f454d28dc
jayson <jayson.santiago1216@gmail.com>
2026-03-16 16:26:58 -0600

    [Application.Units] Introduced GetTenantLookupQuery and its handler to retrieve tenants who do not have an active lease. Utilizes dependency injection for date/time and database context, and maps results to TenantLookupResponse.

---
+++ DongonResidentialsRental.Application/Tenants/Queries/GetTenantsLookup/GetTenantLookupQuery.cs
@@ -0,0 +1,8 @@
+﻿using DongonResidentialsRental.Application.Abstractions.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DongonResidentialsRental.Application.Tenants.Queries.GetTenantsLookup;
+
+public sealed record GetTenantLookupQuery() : IQuery<IReadOnlyList<TenantLookupResponse>>;
---
+++ DongonResidentialsRental.Application/Tenants/Queries/GetTenantsLookup/GetTenantLookupQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using DongonResidentialsRental.Application.Abstractions.Clock;
+using DongonResidentialsRental.Application.Abstractions.Data;
+using DongonResidentialsRental.Application.Abstractions.Messaging;
+using DongonResidentialsRental.Application.Abstractions.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace DongonResidentialsRental.Application.Tenants.Queries.GetTenantsLookup;
+
+public sealed class GetTenantLookupQueryHandler : IQueryHandler<GetTenantLookupQuery, IReadOnlyList<TenantLookupResponse>>
+{
+    private readonly IApplicationDBContext _dbContext;
+    private readonly IDateTimeProvider _dateTimeProvider;
+    public GetTenantLookupQueryHandler(
+        IDateTimeProvider dateTimeProvider,
+        IApplicationDBContext dbContext)
+    {
+        _dateTimeProvider = dateTimeProvider;
+        _dbContext = dbContext;
+    }
+    public async Task<IReadOnlyList<TenantLookupResponse>> Handle(GetTenantLookupQuery query, CancellationToken cancellationToken)
+    {
+        var today = DateOnly.FromDateTime(_dateTimeProvider.Today);
+        // Fetch tenants that doesn't have active lease
+        var lookup = await _dbContext.Tenants
+            .Where(t => 
+                !_dbContext.Leases.Any(
+                    l => l.Occupancy == t.TenantId && 
+                    l.Term.StartDate <= today &&
+                    l.Term.EndDate >= today))
+            .Select(TenantMappings.ToLookupResponse())
+            .ToListAsync(cancellationToken);
+
+        return lookup;
+    }
+}
