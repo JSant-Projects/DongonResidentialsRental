47ad820e58330de81a92fdffaaf4be3eaf4dee41
jayson <jayson.santiago1216@gmail.com>
2026-03-16 18:37:21 -0600

    [Application.Leases] Introduced ActivateLeaseCommand, handler, and validator to support lease activation using CQRS. Includes validation for lease ID and not-found handling. Improves modularity and domain operation structure.

---
+++ DongonResidentialsRental.Application/Leases/Commands/ActivateLease/ActivateLeaseCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DongonResidentialsRental.Application.Leases.Commands.ActivateLease;
+
+public class ActivateLeaseCommandValidator : AbstractValidator<ActivateLeaseCommand>
+{
+    public ActivateLeaseCommandValidator()
+    {
+        RuleFor(x => x.LeaseId.Id)
+            .NotEmpty();
+    }
+}
