686060261639c13a93c020ef4ffac9fd26b35944
jayson <jayson.santiago1216@gmail.com>
2026-03-16 18:00:40 -0600

    [Application.Buildings][Application.Tenants][Application.Units] Introduced FluentValidation classes for GetBuildingsQuery, GetTenantsQuery, and GetUnitsQuery. Each validator ensures Page > 0 and PageSize > 0 and <= 100, improving input validation for paginated queries.

---
+++ DongonResidentialsRental.Application/Buildings/Queries/GetBuildings/GetBuildingQueryValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DongonResidentialsRental.Application.Buildings.Queries.GetBuildings;
+
+public sealed class GetBuildingQueryValidator : AbstractValidator<GetBuildingsQuery>
+{
+    public GetBuildingQueryValidator()
+    {
+        RuleFor(x => x.Page)
+           .GreaterThan(0);
+
+        RuleFor(x => x.PageSize)
+            .GreaterThan(0)
+            .LessThanOrEqualTo(100);
+    }
+}
---
+++ DongonResidentialsRental.Application/Tenants/Queries/GetTenants/GetTenantsQueryValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DongonResidentialsRental.Application.Tenants.Queries.GetTenants;
+
+public sealed class GetTenantsQueryValidator : AbstractValidator<GetTenantsQuery>
+{
+    public GetTenantsQueryValidator()
+    {
+        RuleFor(x => x.Page)
+           .GreaterThan(0);
+
+        RuleFor(x => x.PageSize)
+            .GreaterThan(0)
+            .LessThanOrEqualTo(100);
+    }
+}
