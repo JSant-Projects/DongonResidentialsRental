e05ceaafc6c2a483084601be893e36096d3ba7fe
jayson <jayson.santiago1216@gmail.com>
2026-03-17 01:48:46 -0600

    [Application.Leases] Introduced ChangeLeaseTermCommand for lease term updates, including handler logic for validation and modification, and a validator to ensure valid start dates. Enables lease term modification functionality.

---
+++ DongonResidentialsRental.Application/Leases/Commands/ChangeLeaseTerm/ChangeLeaseTermCommand.cs
@@ -0,0 +1,12 @@
+﻿using DongonResidentialsRental.Application.Abstractions.Messaging;
+using DongonResidentialsRental.Domain.Lease;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DongonResidentialsRental.Application.Leases.Commands.ChangeLeaseTerm;
+
+public sealed record ChangeLeaseTermCommand(
+    LeaseId LeaseId, 
+    DateOnly NewStartDate, 
+    DateOnly? NewEndDate) : ICommand<Unit>;
---
+++ DongonResidentialsRental.Application/Leases/Commands/ChangeLeaseTerm/ChangeLeaseTermCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace DongonResidentialsRental.Application.Leases.Commands.ChangeLeaseTerm;
+
+public sealed class ChangeLeaseTermCommandValidator : AbstractValidator<ChangeLeaseTermCommand>
+{
+    public ChangeLeaseTermCommandValidator()
+    {
+        RuleFor(x => x.NewStartDate)
+            .NotEqual(default(DateOnly));
+    }
+}
