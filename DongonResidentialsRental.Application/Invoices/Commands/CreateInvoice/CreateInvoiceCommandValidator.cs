01a786ed2836ac34e55b08a61aba584eb3188f2a
jayson <jayson.santiago1216@gmail.com>
2026-03-17 20:25:36 -0600

    [Application.Invoices] Implemented CreateInvoiceCommand

---
+++ DongonResidentialsRental.Application/Invoices/Commands/CreateInvoice/CreateInvoiceCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace DongonResidentialsRental.Application.Invoices.Commands.CreateInvoice;
+
+public sealed class CreateInvoiceCommandValidator : AbstractValidator<CreateInvoiceCommand>
+{
+    public CreateInvoiceCommandValidator()
+    {
+        
+    }
+}
