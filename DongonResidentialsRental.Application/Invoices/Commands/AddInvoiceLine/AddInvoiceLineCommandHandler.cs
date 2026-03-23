59c448287e2cfc2a5e5864f29cc1d6b2099a06c5
jayson <jayson.santiago1216@gmail.com>
2026-03-23 17:31:18 -0600

    [Application.Invoices] Implemented AddInvoiceLineCommand

---
+++ DongonResidentialsRental.Application/Invoices/Commands/AddInvoiceLine/AddInvoiceLineCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using DongonResidentialsRental.Application.Abstractions.Messaging;
+using DongonResidentialsRental.Application.Abstractions.Persistence;
+using DongonResidentialsRental.Application.Exceptions;
+using DongonResidentialsRental.Domain.Invoice;
+using DongonResidentialsRental.Domain.Shared;
+
+namespace DongonResidentialsRental.Application.Invoices.Commands.AddInvoiceLine;
+
+public sealed class AddInvoiceLineCommandHandler : ICommandHandler<AddInvoiceLineCommand, Unit>
+{
+    private readonly IInvoiceRepository _invoiceRepository;
+    public AddInvoiceLineCommandHandler(IInvoiceRepository invoiceRepository)
+    {
+        _invoiceRepository = invoiceRepository;
+    }
+    public async Task<Unit> Handle(AddInvoiceLineCommand request, CancellationToken cancellationToken)
+    {
+        var invoice = await _invoiceRepository.GetByIdAsync(request.InvoiceId);
+
+        if (invoice is null)
+        {
+            throw new NotFoundException(nameof(Invoice), request.InvoiceId);
+        }
+
+        var unitPrice = Money.Create(invoice.Currency, request.Price);
+
+        invoice.AddLine(request.Description, request.Quantity, unitPrice, request.LineType);
+
+        return Unit.Value;
+    }
+}
---
+++ DongonResidentialsRental.Application/Invoices/Commands/AddInvoiceLine/AddInvoiceLineCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace DongonResidentialsRental.Application.Invoices.Commands.AddInvoiceLine;
+
+public sealed class AddInvoiceLineCommandValidator : AbstractValidator<AddInvoiceLineCommand>
+{
+    public AddInvoiceLineCommandValidator()
+    {
+        RuleFor(x => x.InvoiceId.Id)
+            .NotEmpty();
+
+        RuleFor(x => x.Description)
+            .NotEmpty()
+            .MaximumLength(200);
+
+        RuleFor(x => x.Price)
+            .NotNull()
+            .GreaterThan(0m);
+    }
+}
