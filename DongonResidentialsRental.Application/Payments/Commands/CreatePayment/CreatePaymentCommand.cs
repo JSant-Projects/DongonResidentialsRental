a94c8e8f10c5d2638117fa7f156bc6142c040912
jayson <jayson.santiago1216@gmail.com>
2026-03-23 16:35:25 -0600

    [Application.Payment] Implemented CreatePaymentCommand

---
+++ DongonResidentialsRental.Application/Payments/Commands/CreatePayment/CreatePaymentCommand.cs
@@ -0,0 +1,16 @@
+﻿using DongonResidentialsRental.Application.Abstractions.Messaging;
+using DongonResidentialsRental.Domain.Payment;
+using DongonResidentialsRental.Domain.Tenant;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DongonResidentialsRental.Application.Payments.Commands.CreatePayment;
+
+public sealed record CreatePaymentCommand(
+    TenantId TenantId,
+    Decimal Amount,
+    string Currency,
+    DateOnly ReceivedOn,
+    PaymentMethod PaymentMethod,
+    string? Reference) : ICommand<PaymentId>;
---
+++ DongonResidentialsRental.Application/Payments/Commands/CreatePayment/CreatePaymentCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using DongonResidentialsRental.Application.Abstractions.Messaging;
+using DongonResidentialsRental.Application.Abstractions.Persistence;
+using DongonResidentialsRental.Application.Exceptions;
+using DongonResidentialsRental.Domain.Payment;
+using DongonResidentialsRental.Domain.Shared;
+using DongonResidentialsRental.Domain.Tenant;
+
+namespace DongonResidentialsRental.Application.Payments.Commands.CreatePayment;
+
+public sealed class CreatePaymentCommandHandler : ICommandHandler<CreatePaymentCommand, PaymentId>
+{
+    private readonly IPaymentRepository _paymentRepository;
+    private readonly ITenantRepository _tenantRepository;
+    public CreatePaymentCommandHandler(
+        IPaymentRepository paymentRepository,
+        ITenantRepository tenantRepository)
+    {
+        _paymentRepository = paymentRepository;
+        _tenantRepository = tenantRepository;
+    }
+    public async Task<PaymentId> Handle(CreatePaymentCommand request, CancellationToken cancellationToken)
+    {
+        var tenant = await _tenantRepository.GetByIdAsync(request.TenantId);
+
+        if (tenant is null)
+        {
+            throw new NotFoundException(nameof(Tenant), request.TenantId);
+        }
+
+        var paymentAmount = Money.Create(request.Currency, request.Amount);
+
+        var payment = Payment.Create(
+            tenant.TenantId, 
+            paymentAmount, 
+            request.ReceivedOn, 
+            request.PaymentMethod, 
+            request.Reference);
+
+        _paymentRepository.Add(payment);
+
+        return payment.PaymentId;
+    }
+}
