4ccd342ff31a055df6dfeaeb1c19f941005245ad
jayson <jayson.santiago1216@gmail.com>
2026-03-24 16:28:28 -0600

    [Application.Payments] Implemented ReversePayment

---
+++ DongonResidentialsRental.Application/Payments/Commands/ReversePayment/ReversePaymentCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace DongonResidentialsRental.Application.Payments.Commands.ReversePayment;
+
+public sealed class ReversePaymentCommandValidator : AbstractValidator<ReversePaymentCommand>
+{
+    public ReversePaymentCommandValidator()
+    {
+        RuleFor(x => x.PaymentId.Id)
+            .NotEmpty();
+
+        RuleFor(x => x.InvoiceId.Id)
+            .NotEmpty();
+
+        RuleFor(x => x.Reason)
+            .NotEmpty();
+    }
+}
