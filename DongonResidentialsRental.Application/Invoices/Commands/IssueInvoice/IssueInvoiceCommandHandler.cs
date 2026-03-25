d04418eaf2aff39e44fa92124940f61abac162dd
jayson <jayson.santiago1216@gmail.com>
2026-03-25 15:49:03 -0600

    [Application.Invoices] Implemented IssueInvoiceCommand.

---
+++ DongonResidentialsRental.Application/Invoices/Commands/IssueInvoice/IssueInvoiceCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using DongonResidentialsRental.Application.Abstractions.Clock;
+using DongonResidentialsRental.Application.Abstractions.Messaging;
+using DongonResidentialsRental.Application.Abstractions.Persistence;
+using DongonResidentialsRental.Application.Exceptions;
+using DongonResidentialsRental.Domain.Invoice;
+
+namespace DongonResidentialsRental.Application.Invoices.Commands.IssueInvoice;
+
+public sealed class IssueInvoiceCommandHandler : ICommandHandler<IssueInvoiceCommand, Unit>
+{
+    private readonly IInvoiceRepository _invoiceRepository;
+    private readonly IDateTimeProvider _dateTimeProvider;
+    public IssueInvoiceCommandHandler(
+        IInvoiceRepository invoiceRepository,
+        IDateTimeProvider dateTimeProvider)
+    {
+        _invoiceRepository = invoiceRepository;
+        _dateTimeProvider = dateTimeProvider;
+    }
+    public async Task<Unit> Handle(IssueInvoiceCommand request, CancellationToken cancellationToken)
+    {
+        var today = DateOnly.FromDateTime(_dateTimeProvider.Today);
+
+        var invoice = await _invoiceRepository.GetByIdAsync(request.InvoiceId);
+        if (invoice is null)
+        {
+            throw new NotFoundException(nameof(Invoice), request.InvoiceId);
+        }
+
+        invoice.Issue(today);
+
+        return Unit.Value;
+    }
+}
---
+++ DongonResidentialsRental.Application/Invoices/Commands/IssueInvoice/IssueInvoiceCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace DongonResidentialsRental.Application.Invoices.Commands.IssueInvoice;
+
+public sealed class IssueInvoiceCommandValidator : AbstractValidator<IssueInvoiceCommand>
+{
+    public IssueInvoiceCommandValidator()
+    {
+        RuleFor(x => x.InvoiceId.Id)
+            .NotEmpty();
+    }
+}
