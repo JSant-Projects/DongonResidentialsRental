e9c969b6ae1ed4620d42ceda74195f5099a7e180
jayson <jayson.santiago1216@gmail.com>
2026-03-25 15:39:46 -0600

    [Application.CreditNotes] Implemented CreateCreditNoteCommand, ApplyCredtiToInvoiceCommand and IssueCreditNoteCommand.

---
+++ DongonResidentialsRental.Application/CreditNotes/Commands/CreateCreditNote/CreateCreditNoteCommand.cs
@@ -0,0 +1,13 @@
+﻿using DongonResidentialsRental.Application.Abstractions.Messaging;
+using DongonResidentialsRental.Domain.CreditNote;
+using DongonResidentialsRental.Domain.Lease;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DongonResidentialsRental.Application.CreditNotes.Commands.CreateCreditNote;
+
+public sealed record CreateCreditNoteCommand(
+    LeaseId LeaseId,
+    decimal Amount,
+    string Currency) : ICommand<CreditNoteId>;
