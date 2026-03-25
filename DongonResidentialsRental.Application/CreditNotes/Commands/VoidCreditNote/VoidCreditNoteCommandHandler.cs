9951157d0e48dc206f60dc31dca75743ea5ae451
jayson <jayson.santiago1216@gmail.com>
2026-03-25 16:24:51 -0600

    [Application.CreditNotes] Implemented VoidCreditNoteCommand.

---
+++ DongonResidentialsRental.Application/CreditNotes/Commands/VoidCreditNote/VoidCreditNoteCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using DongonResidentialsRental.Application.Abstractions.Messaging;
+using DongonResidentialsRental.Application.Abstractions.Persistence;
+using DongonResidentialsRental.Application.Exceptions;
+using DongonResidentialsRental.Domain.CreditNote;
+
+namespace DongonResidentialsRental.Application.CreditNotes.Commands.VoidCreditNote;
+
+public sealed class VoidCreditNoteCommandHandler : ICommandHandler<VoidCreditNoteCommand, Unit>
+{
+    private readonly ICreditNoteRepository _creditNoteRepository;
+    public VoidCreditNoteCommandHandler(ICreditNoteRepository creditNoteRepository)
+    {
+        _creditNoteRepository = creditNoteRepository;
+    }
+    public async Task<Unit> Handle(VoidCreditNoteCommand request, CancellationToken cancellationToken)
+    {
+        var creditNote = await _creditNoteRepository.GetByIdAsync(request.CreditNoteId);
+        if (creditNote is null)
+        {
+            throw new NotFoundException(nameof(CreditNote), request.CreditNoteId);
+        }
+
+        creditNote.Void(); 
+
+        return Unit.Value;
+    }
+}
