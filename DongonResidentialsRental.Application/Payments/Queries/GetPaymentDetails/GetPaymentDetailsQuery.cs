88529ff29d5d865a9baa4b219d7f32ae4af00b13
jayson <jayson.santiago1216@gmail.com>
2026-03-25 14:45:48 -0600

    [Application.Payments] Moved GetPaymentDetailsQueryHandler into its own file. Fixed potential bug query is not filtered by PaymentId.

--- DongonResidentialsRental.Application/Payments/Queries/GetPaymentDetails/GetPaymentDetailsQuery.cs
+++ DongonResidentialsRental.Application/Payments/Queries/GetPaymentDetails/GetPaymentDetailsQuery.cs
@@ -1,9 +1,6 @@
-﻿using DongonResidentialsRental.Application.Abstractions.Data;
-using DongonResidentialsRental.Application.Abstractions.Messaging;
-using DongonResidentialsRental.Application.Exceptions;
+﻿using DongonResidentialsRental.Application.Abstractions.Messaging;
 using DongonResidentialsRental.Domain.Invoice;
 using DongonResidentialsRental.Domain.Payment;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.Tracing;
@@ -12,61 +9,3 @@ using System.Text;
 namespace DongonResidentialsRental.Application.Payments.Queries.GetPaymentDetailsQuery;
 
 public sealed record GetPaymentDetailsQuery(PaymentId PaymentId) : IQuery<PaymentDetailsResponse>;
-
-public sealed class GetPaymentDetailsQueryHandler : IQueryHandler<GetPaymentDetailsQuery, PaymentDetailsResponse>
-{
-    private readonly IApplicationDBContext _dbContext;
-    public GetPaymentDetailsQueryHandler(IApplicationDBContext dbContext)
-    {
-        _dbContext = dbContext;
-    }
-    public async Task<PaymentDetailsResponse> Handle(GetPaymentDetailsQuery request, CancellationToken cancellationToken)
-    {
-        var payment = await (
-            from p in _dbContext.Payments.AsNoTracking()
-            join t in _dbContext.Tenants.AsNoTracking()
-                on p.TenantId equals t.TenantId
-            select new PaymentResponse(
-                p.PaymentId.Id,
-                t.PersonalInfo.FirstName + " " + t.PersonalInfo.LastName,
-                p.Amount.Amount,
-                p.Amount.Currency,
-                p.ReceivedOn,
-                p.Reference,
-                p.Method))
-            .FirstOrDefaultAsync(cancellationToken);
-
-        if (payment is null)
-        {
-            throw new NotFoundException(nameof(Payment), request.PaymentId);
-        }
-
-        var allocations = await GetAllocationsAsync(request.PaymentId, cancellationToken);
-
-        return new PaymentDetailsResponse(
-            payment.PaymentId,
-            payment.TenantName,
-            payment.Amount,
-            payment.Currency,
-            payment.ReceivedOn, 
-            payment.Reference,
-            payment.Method,
-            allocations);
-    }
-
-    private async Task<IReadOnlyList<PaymentAllocationResponse>> GetAllocationsAsync(
-        PaymentId PaymentId, 
-        CancellationToken cancellationToken)
-    {
-        return await (
-            from p in _dbContext.PaymentsAllocations.AsNoTracking()
-            join i in _dbContext.Invoices.AsNoTracking()
-                on p.InvoiceId equals i.InvoiceId
-            select new PaymentAllocationResponse(
-                i.InvoiceNumber,
-                p.Amount.Amount,
-                p.Amount.Currency,
-                p.AllocatedOn))
-            .ToListAsync(cancellationToken);
-    }
-}
