073e336ba7133c05d4c2053827473506a8c49d35
jayson <jayson.santiago1216@gmail.com>
2026-03-24 18:16:09 -0600

    [Application.Payments] Implemented GetPaymentDetailsQuery

---
+++ DongonResidentialsRental.Application/Payments/Queries/GetPaymentDetails/PaymentAllocationResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DongonResidentialsRental.Application.Payments.Queries.GetPaymentDetailsQuery;
+
+public sealed record PaymentAllocationResponse(
+    string InvoiceNumber,
+    decimal Amount,
+    string Currency,
+    DateOnly AllocatedOn);
