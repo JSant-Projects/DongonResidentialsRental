0419a036857c36934f5c4f344117c6d4b9fb9d74
jayson <jayson.santiago1216@gmail.com>
2026-03-25 16:02:49 -0600

    [Application.Invoices] Implemented CancelInvoiceCommand.

---
+++ DongonResidentialsRental.Application/Invoices/Commands/CancelInvoice/CancelInvoiceCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace DongonResidentialsRental.Application.Invoices.Commands.CancelInvoice;
+
+public sealed class CancelInvoiceCommandValidator : AbstractValidator<CancelInvoiceCommand>
+{
+    public CancelInvoiceCommandValidator()
+    {
+        RuleFor(x => x.InvoiceId.Id)
+           .NotEmpty();
+    }
+}
