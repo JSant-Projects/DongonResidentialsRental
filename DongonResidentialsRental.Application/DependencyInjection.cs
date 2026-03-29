67a163cec4ca1541e8a52d1986f56df27bd11daf
jayson <jayson.santiago1216@gmail.com>
2026-03-29 17:11:59 -0600

    [Application] Added DependecyInjection

---
+++ DongonResidentialsRental.Application/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using DongonResidentialsRental.Application.Abstractions.Messaging;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DongonResidentialsRental.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = typeof(DependencyInjection).Assembly;
+
+        services.AddValidatorsFromAssembly(assembly);
+
+        services.Scan(scan => scan
+            .FromAssemblies(assembly)
+            .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<,>)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime()
+            .AddClasses(classes => classes.AssignableTo(typeof(IQueryHandler<,>)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+
+        return services;
+    }
+}
