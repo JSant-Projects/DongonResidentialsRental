e4e567b40fe1b754c89d4fe2f96a77cdfad06a31
jayson <jayson.santiago1216@gmail.com>
2026-03-31 15:46:29 -0600

    [Infrastracture.Clock] Added Infrastructure layer. Added IDateTimeProvider implementation.

---
+++ DongonResidentialsRental.Infrastracture/Clock/DateTimeProvider.cs
@@ -0,0 +1,11 @@
+﻿using DongonResidentialsRental.Application.Abstractions.Clock;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DongonResidentialsRental.Infrastracture.Clock;
+
+internal sealed class DateTimeProvider : IDateTimeProvider
+{
+    public DateOnly Today => DateOnly.FromDateTime(DateTime.UtcNow);
+}
---
+++ DongonResidentialsRental.Infrastracture/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using DongonResidentialsRental.Application.Abstractions.Clock;
+using DongonResidentialsRental.Infrastracture.Clock;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DongonResidentialsRental.Infrastracture;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+
+        return services;
+    }
+}
